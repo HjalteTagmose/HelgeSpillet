cb1c5e1b1db9c583d6ae46f5c3f14cc1b318b35a
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2023-06-12 14:06:48 +0200

    keep direction

--- Player.gd
+++ Player.gd
@@ -6,6 +6,7 @@ const ANGULAR_ACCELERATION = 10
 
 var mesh : Node3D
 var rot = 0.0
+var target_rot = 0.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -26,16 +27,18 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		target_rot = atan2(direction.x, direction.z)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	rot = lerp_angle(rot, atan2(velocity.x, velocity.z), delta * ANGULAR_ACCELERATION)
+	
+	rot = lerp_angle(rot, target_rot, delta * ANGULAR_ACCELERATION)
 	mesh.rotation.y = rot
 	move_and_slide()
 
-	print(rot)
+	print(rad_to_deg(target_rot))
 	
